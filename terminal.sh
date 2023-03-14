747595b3271b072830793767b46f390666d8081b
Randok <97338807+NodeVonHydra@users.noreply.github.com>
2023-03-14 18:14:04 +0100

    Create terminal.sh

---
+++ terminal.sh
@@ -0,0 +1,25 @@
+! solarized theme
+! foreground/background
+! black
+
+! Restore Ctrl+Shift+(c|v)
+URxvt.keysym.Shift-Control-V: eval:paste_clipboard
+URxvt.keysym.Shift-Control-C: eval:selection_to_clipboard
+URxvt.iso14755: false
+URxvt.iso14755_52: false
+URxvt.scrollBar: false
+URxvt.cursorColor: #18B218
+URxvt.cursorBlink: true
+
+URxvt*background: [90]#000000
+URxvt*depth: 32
+URxvt*foreground: #18B218
+
+! green
+URxvt*color2:  #18B218
+URxvt*color10: #FFFFFF
+
+URxvt.font: xft:Monospace:pixelsize=21:antialias=true
+URxvt.boldFont: xft:Monospace-Bold:pixelsize=21:antialias=true
+URxvt.italicFont: xft:Monospace:pixelsize=21:antialias=true
+URxvt.letterSpace: -1
